0977cf2250d807339192792cee734f22c07af896
Gianelle Tianco <gianelle.tianco@gmail.com>
2021-10-11 20:21:25 -0400

    Added  other js

---
+++ models/connection.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connection
+    .on("open", () => console.log("Connected to Mongo"))
+    .on("close", () => console.log("Disconnected from Mongo"))
+    .on("error", (error) => console.log(error))
+
+module.exports = mongoose
